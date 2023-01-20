e37d7e42a61c8c96b6b32b94dd2a78ec6a85f9ae
Miheer Vaidya <m.vaidya@utah.edu>
2023-01-19 22:45:03 -0700

    taco example

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Kokkos_Core.hpp>
+#include <taco.h>
 
 using namespace std;
 
@@ -7,6 +8,7 @@ int main(int argc, char *argv[])
 {
     Kokkos::initialize(argc, argv);
     {
+        auto a = taco::read("494_bus.mtx", taco::COO(2));
         cout << "Hello World!" << endl;
     }
     Kokkos::finalize();
