685074b9d203e8a3495284f5de7117779763b13e
Miheer Vaidya <m.vaidya@utah.edu>
2023-07-23 01:30:49 -0600

    format

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
 #include <Kokkos_Core.hpp>
+#include <iostream>
 #include <taco.h>
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    Kokkos::initialize(argc, argv);
-    {
-        auto a = taco::read("494_bus.mtx", taco::COO(2));
-        cout << "Hello World!" << endl;
-    }
-    Kokkos::finalize();
-    return 0;
+int main(int argc, char *argv[]) {
+  Kokkos::initialize(argc, argv);
+  {
+    assert(argc == 2);
+    auto input_file = argv[1];
+    auto a = taco::read(input_file, taco::COO(2));
+    cout << a << endl;
+  }
+  Kokkos::finalize();
+  return 0;
 }
