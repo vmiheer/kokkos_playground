7a5bed411708f0acd1dfb5f339e0fc9fe222a106
Miheer Vaidya <m.vaidya@utah.edu>
2024-02-15 16:55:15 -0700

    use gcc13

--- env.sh
+++ env.sh
@@ -3,6 +3,8 @@ export WORKSPACE=$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 export Kokkos_ROOT=$WORKSPACE/kokkos_install
 export CMAKE_PREFIX_PATH=$Kokkos_ROOT:$CMAKE_PREFIX_PATH
 
+export CC=`which gcc-13`
+export CXX=`which g++-13`
 git submodule update --init --recursive
 
 cd $WORKSPACE/kokkos
